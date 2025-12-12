84bf8fb1ad8e786fcc1366a415f27be285f3e22b
Kuroi-Kage <walkerlemec@gmail.com>
2025-12-12 19:25:36 +0300

    Ajoute premier version pour l'assistance

---
+++ core/precessor.py
@@ -0,0 +1,18 @@
+class Precessor:
+    # Détecte le type de message
+    QUESTIONS = ["?"]
+    INTERROGATIFS = ["comment", "quoi", "pourquoi", "ou", "qui", "quand"]
+    COMMANDS = ["ouvre", "ferme", "montre", "ajoute", "supprime"]
+    
+    def detect(self, text: str) -> str:
+        text_lower = text.lower().strip()
+        # Vérifier question
+        if "?" in text_lower or any(text_lower.startswith(word) for word in self.INTERROGATIFS ):
+            return "question"
+       # if any(q in text_lower for q in self.QUESTIONS):
+           # return "question"
+        #Vérifier commande
+        if any(text_lower.startswith(cmd) for cmd in self.COMMANDS):
+            return "commande"
+        # sinon message normal
+        return "normal"
